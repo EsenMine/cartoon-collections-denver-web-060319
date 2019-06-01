62239dc86c7eae36baddf9efcde25dea127f4d6a
Miné E <mine_esen@yahoo.ca>
2019-06-01 20:44:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,14 @@ def long_planeteer_calls(calls)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array_of_strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if array_of_strings.include("cheddar")
+    return
+  elsif array_of_strings.include("gouda")
+    return
+  elsif array_of_strings.include("camembert")
+    return
+  else   
+  end
 end
